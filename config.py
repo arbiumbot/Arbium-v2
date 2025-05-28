54cbb1d2c716b523faac209d683533172c246012
arbiumbot <ostapcukdenis02@gmail.com>
2025-05-28 14:16:11 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "7569974982:AAFPljh4Xh-SlLfbS1PwQ06igD90w9i4_7g"
+ADMIN_ID = 648661151
+WEBHOOK_URL = "https://web-production-d485.up.railway.app/webhook/bot/7569974982:AAFPljh4Xh-SlLfbS1PwQ06igD90w9i4_7g"
