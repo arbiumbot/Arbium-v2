f05799253f44af788848b94db5e93c1fdd15657e
arbiumbot <ostapcukdenis02@gmail.com>
2025-05-28 14:17:36 +0300

    Create i18n.py

---
+++ i18n.py
@@ -0,0 +1,23 @@
+import i18n
+
+i18n.translations = {
+    "en": {
+        "arbitrage": "📊 Arbitrage",
+        "chart": "📈 Chart",
+        "history": "🕓 History",
+        "topTokens": "🏅 Top Tokens",
+        "topExchanges": "🏆 Top Exchanges",
+        "settings": "⚙️ Settings"
+    },
+    "ua": {
+        "arbitrage": "📊 Арбітраж",
+        "chart": "📈 Графік",
+        "history": "🕓 Історія",
+        "topTokens": "🏅 Топ монет",
+        "topExchanges": "🏆 Топ бірж",
+        "settings": "⚙️ Налаштування"
+    }
+}
+
+i18n.default_locale = "en"
+i18n.fallbacks = True
