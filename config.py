a674aac67840cc118fb5550c8321c9a7c3c5702f
arbiumbot <ostapcukdenis02@gmail.com>
2025-05-29 16:23:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "7569974982:AAFPljh4Xh-SlLfbS1PwQ06igD90w9i4_7g"
 ADMIN_ID = 648661151
-WEBHOOK_URL = "https://web-production-d485.up.railway.app/webhook/bot/7569974982:AAFPljh4Xh-SlLfbS1PwQ06igD90w9i4_7g"
+WEBHOOK_URL = "WEBHOOK_URL = "https://arbium-v2-production.up.railway.app/webhook/bot/7569974982:AAFPljh4Xh-SlLfbS1PwQ06igD90w9i4_7g"AAFPljh4Xh-SlLfbS1PwQ06igD90w9i4_7g"
